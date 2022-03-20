f8e4d41c84d1bd91937ec7659dd0228870087272
Joao Morgado <joao.morgado@talkdesk.com>
2022-03-20 11:55:34 +0000

    5.2. Api Gateway develop log trace

--- cloud/api-gateway/src/main/java/io/github/jlmc/apigateway/ApiGatewayApplication.java
+++ cloud/api-gateway/src/main/java/io/github/jlmc/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,12 @@
 package io.github.jlmc.apigateway;
 
+import io.netty.handler.logging.LogLevel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @EnableEurekaClient
 
@@ -13,4 +17,13 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    /**
+     * This bean is only used allow us to log the request, to have a better trace and flow understand.
+     */
+    @Bean
+    HttpClient httpClient() {
+        return HttpClient.create()
+                         .wiretap("LoggingFilter",
+                                 LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL);
+    }
 }
